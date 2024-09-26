5ec334f6e6a1651a51dade35d8f00a3c5d256e08
Michael <michaelkinrosslim@gmail.com>
2024-09-26 10:52:53 +0000

    added board class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <unordered_map>
+
+enum class Piece
+{
+	pawn, knight, bishop, rook, queen, king;
+};
+
+class Board_state
+{
+	private:
+	std::unordered_map<Piece, uint64_t> pieces;
+	satic unordered_map<Piece, int> piece_values
+	{
+		{Piece::pawn, 1}, 
+		{Piece::knight, 3},
+		{Piece::bishop, 3}, 
+		{Piece::rook, 5}, 
+		{Piece::queen, 9}
+	};
+
+	[[nodiscard]] const double white_material_value() const;
+	[[nodiscard]] const double black_material_value() const;
+	
+	public:
+	const int size = 64;
+
+	[[nodiscard]] const bool white_is_mated() const;
+	[[nodiscard]] const bool black_is_mated() const;
+	[[nodiscard]] const bool is_draw() const;
+	[[nodiscard]] const double evaluate() const;
+};
 
 int main()
 {
-	std::cout << "alive!!";
+	
 }
