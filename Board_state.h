2c14836bfcee67863d2e85372fe43f48606fe41d
Michael <michaelkinrosslim@gmail.com>
2024-09-27 12:19:14 +0000

    weights

--- Board_state.h
+++ Board_state.h
@@ -12,104 +12,25 @@ enum class Piece
 
 class Board_state
 {
-	private:
-	std::unordered_map<Piece, uint64_t> pieces;
-	static std::unordered_map<Piece, int> piece_values =
-	{
-		{Piece::pawn, 1}, 
-		{Piece::knight, 3},
-		{Piece::bishop, 3}, 
-		{Piece::rook, 5}, 
-		{Piece::queen, 9}
-	};
-	static std::unordered_map<Piece, std::array<std::array<Piece, size>, 5> black_weightmaps;
-	static std::unordered_map<Piece, std::array<std::array<Piece, size>, 5> white_weightmaps = 
-	{
-		{
-			Piece::pawn,
-			 0,  0,  0,  0,  0,  0,  0,  0,
-			 5, 10, 10,-20,-20, 10, 10,  5,
-			 5, -5,-10,  0,  0,-10, -5,  5,
-			 0,  0,  0, 20, 20,  0,  0,  0,
-			 5,  5, 10, 25, 25, 10,  5,  5,
-			 10, 10, 20, 30, 30, 20, 10, 10,
-			 50, 50, 50, 50, 50, 50, 50, 50,
-			 0,  0,  0,  0,  0,  0,  0,  0
-		},
-		{
-			Piece::knight,
-			-50,-40,-30,-30,-30,-30,-40,-50,
-			-40,-20,  0,  5,  5,  0,-20,-40,
-			-30,  5, 10, 15, 15, 10,  5,-30,
-			-30,  0, 15, 20, 20, 15,  0,-30,
-			-30,  5, 15, 20, 20, 15,  5,-30,
-			-30,  0, 10, 15, 15, 10,  0,-30,
-			-40,-20,  0,  0,  0,  0,-20,-40,
-			-50,-40,-30,-30,-30,-30,-40,-50
-		},
-		{
-			Piece::bishop,
-			-20,-10,-10,-10,-10,-10,-10,-20,
-			-10,  5,  0,  0,  0,  0,  5,-10,
-			-10, 10, 10, 10, 10, 10, 10,-10,
-			-10,  0, 10, 10, 10, 10,  0,-10,
-			-10,  5,  5, 10, 10,  5,  5,-10,
-			-10,  0,  5, 10, 10,  5,  0,-10,
-			-10,  0,  0,  0,  0,  0,  0,-10,
-			-20,-10,-10,-10,-10,-10,-10,-20
-		},
-		{
-			Piece::rook,
-			 0,  0,  5,  10, 10, 5,  0,  0,
-			-5,  0,  0,  0,  0,  0,  0, -5,
-			-5,  0,  0,  0,  0,  0,  0, -5,
-			-5,  0,  0,  0,  0,  0,  0, -5,
-			-5,  0,  0,  0,  0,  0,  0, -5,
-			-5,  0,  0,  0,  0,  0,  0, -5,
-			 5,  10, 10, 10, 10, 10, 10, 5,
-			 0,  0,  0,  0,  0,  0,  0,  0,
-
-		},
-		{
-			Piece::queen,
-			-20,-10,-10, -5, -5,-10,-10,-20
-			-10,  0,  5,  0,  0,  0,  0,-10,
-			-10,  5,  5,  5,  5,  5,  0,-10,
-			 0,  0,  5,  5,  5,  5,  0, -5,
-			-5,  0,  5,  5,  5,  5,  0, -5,
-			-10,  0,  5,  5,  5,  5,  0,-10,
-			-10,  0,  0,  0,  0,  0,  0,-10,
-			-20,-10,-10, -5, -5,-10,-10,-20,
-
-		},
-		{
-			Piece::king,
-			 20,  30,  10,  0,   0,   10,  30,  20,
-			 20,  20,  0,   0,   0,   0,   20,  20,
-			-10, -20, -20, -20, -20, -20, -20, -10,
-			-20, -30, -30, -40, -40, -30, -30, -20,
-			-30, -40, -40, -50, -50, -40, -40, -30,
-			-30, -40, -40, -50, -50, -40, -40, -30,
-			-30, -40, -40, -50, -50, -40, -40, -30,
-			-30, -40, -40, -50, -50, -40, -40, -30,
-		},
-	};
-
-	[[nodiscard]] const double white_material_value() const;
-	[[nodiscard]] const double black_material_value() const;
-	void init_black_weightmaps();
-
 	public:
-	const int size = 64;
+		
+	const static int size = 8;
 
 	[[nodiscard]] const bool white_is_mated() const;
 	[[nodiscard]] const bool black_is_mated() const;
 	[[nodiscard]] const bool is_draw() const;
 	[[nodiscard]] const double evaluate() const;
-
-	std::unordered_map<Piece, int>& white_weightmaps() { return white_weightmaps; }
-	std::unordered_map<Piece, int>& black_weightmaps() { return black_weightmaps; }
-	std::unordered_map<Piece, int>& piece_values() { return piece_values; }
+	
+	private:
+	
+	std::unordered_map<Piece, uint64_t> pieces;
+	static std::unordered_map<Piece, int> piece_values;
+	static std::unordered_map<Piece, std::array<std::array<int, size-1>, size-1>> black_weightmaps;
+	static std::unordered_map<Piece, std::array<std::array<int, size-1>, size-1>> white_weightmaps;
+	
+	[[nodiscard]] const double white_material_value() const;
+	[[nodiscard]] const double black_material_value() const;
+	void init_black_weightmaps();
 };
 
 #endif // Board_state_h_INCLUDED
--- main.cpp
+++ main.cpp
@@ -2,14 +2,6 @@
 
 #include "Board_state.h"
 
-void init_weightmaps(Board_state& board_state)
-{
-	board_state.white_weightmaps() = 
-	board_state.piece_values() =
-
-	board_state.black_weightmaps() = std::ranges::reverse(board_state.white_weightmaps);
-}
-
 int main()
 {
     Board_state board_state;
