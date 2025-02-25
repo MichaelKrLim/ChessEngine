d521a760dcb4c4a7ce4728a5221ad82fe69e7c10
Michael Lim <83725834+MichaelKrLim@users.noreply.github.com>
2025-02-25 15:21:46 +0000

    work on tests for 'Move_generator'

--- src/Move_generator.tests.cpp
+++ src/Move_generator.tests.cpp
@@ -2,32 +2,33 @@
 
 #include "Move_generator.h"
 
+#include <cstdio>
+#include <cstring>
+#include <memory>
+#include <optional>
+#include <sstream>
+#include <string>
+
 using namespace engine;
+
+std::optional<std::string> get_diff(const std::string fen, const char depth)
+{
+	const char* const command{std::string{"./perftree ./perft fen "+fen+" depth "+depth}.c_str()};
+	std::unique_ptr<std::FILE, decltype(&pclose)> p{popen(command, "r"), &pclose};
+	if(p.get() == nullptr)
+		throw std::runtime_error{"Move_generator.tests.cpp: 'get_diff': failed to create pipe"};
+	std::string diff(std::istreambuf_iterator<char>(p.get()), {});
+	if(!diff.empty())
+		return std::optional<std::string>{diff};
+	return std::nullopt;
+}
 
 TEST_SUITE("Move_generator")
 {
-	Move_generator move_generator{};
-	TEST_CASE("Validate Reachable Square Generation")
+	TEST_CASE("Diff against stockfish")
 	{
-		SUBCASE("Pawns")
-		{
-			CHECK(1==1);
-		}
-		SUBCASE("Knights")
-		{
-			CHECK(1==1);
-		}
-		SUBCASE("Bishops")
-		{
-			CHECK(1==1);
-		}
-		SUBCASE("Rooks")
-		{
-			CHECK(1==1);
-		}
-		SUBCASE("King")
-		{
-			CHECK(1==1);
-		}
+		Move_generator move_generator{};
+		std::optional<std::string> diff = get_diff("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", '3');
+		CHECK_MESSAGE(!diff.has_value(), diff.value());
 	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,40 +8,6 @@ using namespace engine;
 
 // opening position - "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
-void perft(const Board& board, const int depth, const Move_generator& mg, int current_branches, bool first = false)
-{
-	static int total_branches{0};
-	if(depth == 0)
-	{
-		std::cout << current_branches;
-	}
-	else
-	{
-		for(const auto& pieces : mg.get(board))
-		{
-			for(const auto& [origin_square, destinations] : pieces)
-			{
-				if(first && !destinations.empty())
-					std::cout << origin_square << ' ';
-				for(const auto& destination_square : destinations)
-				{
-					if(first) 
-						std::cout << destination_square;
-					if(!first) 
-					{
-						++current_branches;
-						++total_branches;
-					}
-					Board next_move{board};
-					next_move.make(Move::make(origin_square, destination_square, Move_type::normal));
-					perft(next_move, depth-1, mg, current_branches);
-				}
-			}
-		}
-	}
-	if(first) std::cout << "\n" << total_branches;
-}
-
 int main()
 {
 	[[maybe_unused]] Engine engine;
